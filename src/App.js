b4deafc0098ffcc7079250fabc4eda5f523006e8
RAUMYA KOLEY <raumyakoley@gmail.com>
2024-01-20 20:23:20 +0530

    VOTER DETAILS PAGE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import VoterDetails from './Pages/VoterDetails/VoterDetails.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Hello
-        </a>
-      </header>
+  <div className='App'>
+    <VoterDetails/>
     </div>
-  );
+  )
 }
 
 export default App;
+
