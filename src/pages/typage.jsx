7e43277d20f3eec10730710edbecfb8c696001b2
Upasana Paul <paulupasana18@gmail.com>
2024-01-22 20:48:48 +0530

    Thank you page initial

---
+++ src/pages/typage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Thanks = () =>{
+
+    return(
+        <>
+            <div className="main-div" style={{height: "100vh", position:"fixed", width:"100%", background:"linear-gradient(90deg, rgba(225,239,255,1) 60%, rgba(255,255,255,1) 100%)" }}>
+                <div className="child-div" style={{height:"50vh", width:"30%", backgroundColor:"blue", marginLeft:"25%", marginTop:"10vh"}}></div>
+            </div>
+        </>
+    );
+
+};
+export default Thanks;
