68112282076087d4f4ffee242f73e014b007c579
RAUMYA KOLEY <raumyakoley@gmail.com>
2024-01-25 01:58:14 +0530

    login from page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// import VoterDetails from './Pages/VoterDetails/VoterDetails.jsx';
+import LogIn from "./Pages/LogIn/LogIn.jsx";
+import { BrowserRouter,Routes ,Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Hello
-        </a>
-      </header>
+  <div className='App'>
+    <BrowserRouter>
+    <Routes>
+      {/* <Route path="voterdetails" element={<VoterDetails/>}/> */}
+      <Route path="login" element={<LogIn/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
