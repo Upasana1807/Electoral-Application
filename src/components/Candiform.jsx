2b4890efbc53495ca52aa5de55aebea4cbe18981
Upasana Paul <paulupasana18@gmail.com>
2024-02-01 00:46:50 +0530

    branch of candidate form

---
+++ src/components/Candiform.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Candiform =() =>{
+    return(
+        <>
+            <div style={{height:"10vh", width:"20%", backgroundColor:"red", marginLeft:"5%"}}></div>
+
+        </>
+    );
+};
+
+export default Candiform;
