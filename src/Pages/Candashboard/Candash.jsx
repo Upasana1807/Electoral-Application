fb90a9390a2881f1df61a7062891cc8592144571
Upasana Paul <paulupasana18@gmail.com>
2024-02-01 00:55:35 +0530

    Candidate dashboard branch created

---
+++ src/Pages/Candashboard/Candash.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Candash =() =>{
+    return(
+        <>
+            <div style={{height:"10vh", width:"20%", backgroundColor:"green", marginLeft:"5%"}}></div>
+
+        </>
+    );
+};
+
+export default Candash;
