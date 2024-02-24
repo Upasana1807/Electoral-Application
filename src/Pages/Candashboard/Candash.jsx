94f82767a4cf5cdf9ebe83adfa20a14256a99da7
Snehal bhowmick <snehalbhowmick1@gmail.com>
2024-02-24 10:19:23 +0530

    Candash-board

--- src/Pages/Candashboard/Candash.jsx
+++ src/Pages/Candashboard/Candash.jsx
@@ -1,12 +1,78 @@
-import React from "react";
+import React from 'react'
+import './Candash.css'
+import BG from "../../image/icon.jpg"
+import ICON from "../../Images/icon.png"
 
-const Candash =() =>{
-    return(
-        <>
-            <div style={{height:"10vh", width:"20%", backgroundColor:"green", marginLeft:"5%"}}></div>
+const  Candash = () => {
+    return (
+        <div className='candash-outer-div'>
+            <div className='candash-inner-div'>
+                <div className='candash-inner1'>
+                    <div className='candash-inner-div1'>
+                        <span className='candash-heading'>Candidate Dashbord</span>
+                    </div>
+                    <div className='candash-inner-div2'>
+                        <div className='candash-pic-div'>
+                        <img src={ICON} alt="icon" height={78} width={78}/> 
+                        </div>
+                        <div className='candash-name-div'>
+                            <span className='candash-name'>NARASINGHA CHATTOPADHYAY</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='candash-inner2'>
+                    {/* <div className='inner-box'>
 
-        </>
-    );
-};
+                    </div> */}
+                </div>
+            </div>
+            <div className='candash-inner-div3'>
+                <div className='candash-box'>
+                    <div className='candash-box-img'>
+                    <img src={BG} alt="pic" height={125} width={155}/> 
+                    </div>
+                    <div className='candash-box-text'>
+                    <span className='candash-name1'>NARASINGHA</span>    
+                    </div>
+                </div>
+                <div className='candash-box'>
+                    <div className='candash-box-img'>
+                    <img src={BG} alt="pic" height={125} width={155}/>
+                    </div>
+                    <div className='candash-box-text'>
+                    <span className='candash-name2'>CMP</span>       
+                    </div>
+                </div>
+                <div className='candash-box'>
+                    <div className='candash-box-img'>
+                    <img src={BG} alt="pic" height={125} width={155}/>
+                    </div>
+                    <div className='candash-box-text'>
+                    <span className='candash-name3'>SANDAKFU</span>     
+                    </div>
+                </div>
+                <div className='candash-box'>
+                    <div className='candash-box-img'>
+                    <img src={BG} alt="pic" height={125} width={155}/>
+                    </div>
+                    <div className='candash-box-text'>
+                    <span className='candash-name4'>DOHA</span>     
+                    </div>
+                </div>
+                {/* <div className='box-right'>
+                    <div className='box-img-right'>
 
-export default Candash;
+                    </div>
+                    <div className='box-text-right'>
+                    <span className='name4'>PENDING</span>
+                    </div>
+                </div> */}
+            </div>
+            <div className='candash-button-div'>
+               <button className='candash-but-div'>Log Out</button>
+            </div>
+        </div>
+      )
+  }
+
+  export default Candash
