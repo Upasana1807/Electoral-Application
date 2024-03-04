b987e169da89d1c353dee371b1af295b628ea286
RAUMYA KOLEY <raumyakoley@gmail.com>
2024-03-04 23:38:51 +0530

    admin dashnord 1

--- src/Pages/Admindashboard/Addash.jsx
+++ src/Pages/Admindashboard/Addash.jsx
@@ -1,9 +1,46 @@
 import React from "react";
-
+import "./Addash.css"
+import {Link} from "react-router-dom";
+import Manage from "../../Images/manage.png";
+import Add from "../../Images/add.png";
+import Report from "../../Images/report.png";
+import Logout from "../../Images/logout.png";
 const Addash =() =>{
     return(
         <>
-            <div style={{height:"10vh", width:"20%", backgroundColor:"purple", marginLeft:"5%"}}></div>
+            <div className="admindash_outer">
+            <div className="admindash_heading">
+                <span><b>Admin Dashboard</b></span>
+            </div >
+            <div className="admindash_btn">
+                <Link to="">
+                <button className="main_btn">
+                <img src={Manage} alt="sample" className="btn_img"></img>
+                <p className="btn_name"><b>Manage</b></p>
+                </button>
+                </Link>
+                <Link to="">
+                <button className="main_btn">
+                <img src={Add} alt="sample" className="btn_img"></img>
+                <p className="btn_name"><b>Add Members</b></p>
+                </button>
+                </Link>
+                <Link to="">
+                <button className="main_btn">
+                <img src={Report} alt="sample" className="btn_img"></img>
+                <p className="btn_name"><b>Report</b></p>
+                </button>
+                </Link>
+                <Link to="/">
+                <button className="main_btn">
+                <img src={Logout} alt="sample" className="btn_img"></img>
+                <p className="btn_name"><b>Logout</b></p>
+                </button>
+                </Link>
+            </div>
+        
+           </div>
+           
 
         </>
     );
