9fbc6794a8f9500835f5a386bc5731772935deee
Snehal bhowmick <snehalbhowmick1@gmail.com>
2024-03-05 23:13:19 +0530

    thank you page

--- src/Pages/Typage/Typage.jsx
+++ src/Pages/Typage/Typage.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import './Typage.css';
+import TYPIC from "../../image/typic.png"
 
 const Typage =() =>{
     return(
-        <>
-            <div style={{height:"10vh", width:"20%", backgroundColor:"red", marginLeft:"5%"}}></div>
-
-        </>
+        <div className="ty-outer-div">
+            <div className="ty-heading-div">
+               <h1 className="ty-heading-name-div">Thank you for casting your vote!</h1>
+            </div>
+            <div className="ty-pic-div">
+            <img src={TYPIC} alt="typic" height={330} width={520} /> 
+            </div>
+            <button className="tybutton">
+                <span className="tybutton-text">Log Out</span>
+            </button>
+        </div>
     );
 };
 
